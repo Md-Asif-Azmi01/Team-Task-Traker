2de7a1a4fc3272ff1dbb891f7774f56ce1b98e50
Md Asif Azmi <mdasifazmi01@gmail.com>
2026-05-20 16:43:32 +0530

    ....

--- Backend/app.js
+++ Backend/app.js
@@ -8,7 +8,22 @@ const userRoutes = require('./routes/users');
 const taskRoutes = require('./routes/tasks');
 
 const app = express();
-app.use(cors());
+
+const allowedOrigins = [
+  'http://localhost:5173',                
+  'https://your-frontend.onrender.com'   
+];
+
+app.use(cors({
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  }
+}));
+
 app.use(express.json());
 
 mongoose.connect(process.env.MONGO_URI)
